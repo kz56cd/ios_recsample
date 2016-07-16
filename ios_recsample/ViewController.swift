cb0a20e273d1e90e1f725325aa2a25f74f6cf2f3
kz56cd <kz56cd@gmail.com>
2016-07-16 18:59:14 +0900

    Add ios prj

---
+++ ios_recsample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_recsample
+//
+//  Created by 佐野正和 on 2016/07/16.
+//  Copyright © 2016年 佐野正和. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
